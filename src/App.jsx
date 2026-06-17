b16303b380df25966482007ff0fbcf1d2cc5c763
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2026-06-17 03:07:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,73 @@
-{
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core/dist/cjs/popper.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Componentes base
+import MenuPublico from './componentes/MenuPublico';
+import MenuPrivado from './componentes/MenuPrivado';
+import Login from './componentes/telas/login/Login';
+
+// Telas
+import Home from './componentes/telas/Home';
+import Sobre from './componentes/telas/Sobre';
+
+// CRUD
+import Cantor from './componentes/telas/cantor/Cantor';
+import Genero from './componentes/telas/genero/Genero';
+import Gravadora from './componentes/telas/gravadora/Gravadora';
+import Musica from './componentes/telas/musica/Musica';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MenuPublico />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "sobre",
+        element: <Sobre />
+      },
+      {
+        path: "login",
+        element: <Login />
+      }
+    ]
+  },
+  {
     path: "/Privado",
     element: <MenuPrivado />,
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <Home />
       },
       {
         path: "cantores",
-        element: <Cantor />,
+        element: <Cantor />
       },
       {
         path: "generos",
-        element: <Genero />,
+        element: <Genero />
       },
       {
         path: "gravadoras",
-        element: <Gravadora />,
+        element: <Gravadora />
       },
       {
         path: "musicas",
-        element: <Musica />,
+        element: <Musica />
       }
     ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
